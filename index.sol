8da6eccd9045226cd2299848e10998589a99ccbb
bobcoi03 <elongatedmusk6969@gmail.com>
2023-01-07 19:38:38 +0000

    l

--- index.sol
+++ index.sol
@@ -1,17 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
-contract SimpleStorage {
-    uint storedData;
-
-    function set(uint x) public {
-        storedData = x;
-    }
-    function get() public view returns (uint) {
-        return storedData;
-    }
-}
-
 contract Coin {
     address public minter;
     mapping (address => uint) public balances;
